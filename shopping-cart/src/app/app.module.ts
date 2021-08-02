d4e4612f04574a295784419ed95f62669439b048
Jesper Bengtsson <jesperbengtsson2@gmail.com>
2021-08-02 11:32:24 +0200

    Initial commit

---
+++ shopping-cart/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
+import { ProductsComponent } from './components/shopping-cart/products/products.component';
+import { ProductItemComponent } from './components/shopping-cart/products/product-item/product-item.component';
+import { CartComponent } from './components/shopping-cart/cart/cart.component';
+import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent,
+    ShoppingCartComponent,
+    FiltersComponent,
+    ProductsComponent,
+    ProductItemComponent,
+    CartComponent,
+    CartItemComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
